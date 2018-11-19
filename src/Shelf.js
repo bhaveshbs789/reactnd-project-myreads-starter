9757af6f0ef4f75d75dbce742e356f05e9ff98fb
bhaveshbs789 <bhaveshbs789@gmail.com>
2018-11-19 14:47:15 +0530

    Updated Prop changeShelf for Book component

--- src/Shelf.js
+++ src/Shelf.js
@@ -3,14 +3,14 @@ import Book from './Book'
 
 class Shelf extends Component {
 	render() {
-        console.log(this.props);
+        //console.log('Props from ListBooks', this.props);
 		return (
 		    <div className="bookshelf">
               <h2 className="bookshelf-title">{this.props.section}</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
                   {this.props.books.map((book) => (
-                        <Book key={book.id} bookStatus={book}/>
+                        <Book key={book.id} bookStatus={book} changeShelf={this.props.onChangeShelf}/>
                     )                
                   )}                  
                 </ol>
