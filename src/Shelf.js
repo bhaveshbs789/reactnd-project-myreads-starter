18fb5530fbb1f2972c26ecdefbe5b702d396f53e
bhaveshbs789 <bhaveshbs789@gmail.com>
2018-11-19 10:51:38 +0530

    Added key to Book component
    Book id added as unique key to ordered list

--- src/Shelf.js
+++ src/Shelf.js
@@ -3,12 +3,16 @@ import Book from './Book'
 
 class Shelf extends Component {
 	render() {
+        console.log(this.props);
 		return (
 		    <div className="bookshelf">
               <h2 className="bookshelf-title">{this.props.section}</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  <Book bookStatus={this.props.books}/>
+                  {this.props.books.map((book) => (
+                        <Book key={book.id} bookStatus={book}/>
+                    )                
+                  )}                  
                 </ol>
               </div>
             </div>
