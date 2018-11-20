8b8876dcd060dd64b4ff00141f1cf882637d5724
bhaveshbs789 <bhaveshbs789@gmail.com>
2018-11-20 14:53:24 +0530

    Search route updated with one Prop
    myBooksList prop passed to Search component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
 import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
-// import Shelf from './Shelf'
 import Search from './Search'
 import ListBooks from './ListBooks'
-import {Switch, Route} from 'react-router-dom'
+import {Route} from 'react-router-dom'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    //showSearchPage: false,
     books : []
   }
 
+  //---Run componentDidMount when render is just done---    
   componentDidMount(){
     BooksAPI.getAll()
     .then((books) => {
-//      console.log(books);
       this.setState({books:books})
     })
+  }
+
+  updateBookShelf = (book, shelf) => {
+      BooksAPI.update(book, shelf)
+      .then(() => {
+          BooksAPI.getAll()
+          .then((books) => {
+              this.setState({books : books})
+          })
+      })
   }
 
   render() {
     return (
       <div className="app">
-        <Switch>
           <Route exact path='/' render={() => (
-              <ListBooks booksOnShelf={this.state.books}/>
+              <ListBooks booksOnShelf={this.state.books} onShelfChange={this.updateBookShelf}/>
             )}/>
-          <Route exact path='/search' component={Search}/>
-        </Switch>
+          <Route exact path='/search' render={({history}) => (<Search myBooksList={this.state.books} />)}/>
       </div>
     )
   }
