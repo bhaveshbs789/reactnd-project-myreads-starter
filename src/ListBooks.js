8e17c7004eddc0f5886943b96d9ba730b881267a
bhaveshbs789 <bhaveshbs789@gmail.com>
2018-11-20 14:54:28 +0530

    Prop names for Shelf component updated
    onChange Prop updated

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Shelf from './Shelf'
-import * as BooksAPI from './BooksAPI'
+//import * as BooksAPI from './BooksAPI'
+import { Link } from 'react-router-dom'
+import './App.css'
 
 class ListBooks extends Component {
     
@@ -8,47 +10,34 @@ class ListBooks extends Component {
         books : []
     };
 
-    handleChangeShelf = (book, shelf) => {
-//        const book = this.props.booksOnShelf.filter((b) => b.id === bookId)[0]; // returns the first book object
-//        console.log(book);
-        BooksAPI.update(book.id,shelf)
-        .then((response) => {
-            book.shelf = shelf;
-            this.setState(state => ({
-                books: state.books.filter(bo => bo.id !== book.id).concat([book])
-            }));
-        });
-    }
-    
-    
 	render() {
 		return (
 		<div className="list-books">
-            	    <div className="list-books-title">
-                    <h1>MyReads</h1>
-            	</div>
-            	<div className="list-books-content">
-              	    <div>
-                    <Shelf section="Currently Reading"
-                    books={this.props.booksOnShelf.filter((book) => book.shelf === "currentlyReading")}
-                    onChangeShelf={this.handleChangeShelf}
-                    />                
-                    <Shelf section="Want to Read"
-                    books={this.props.booksOnShelf.filter((book) => book.shelf === "wantToRead")}
-                    onChangeShelf={this.handleChangeShelf}
-                    />                
-                    <Shelf section="Read"
-                    books={this.props.booksOnShelf.filter((book) => book.shelf === "read")}
-                    onChangeShelf={this.handleChangeShelf}
-                    />                
-                </div>
-               </div>
-                <div className="open-search">
-                    <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
-                </div>
-               </div>
-	     )
-       }
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                <Shelf section="Currently Reading"
+                books={this.props.booksOnShelf.filter((book) => book.shelf === "currentlyReading")}
+                onChange={this.props.onShelfChange}
+                />                
+                <Shelf section="Want to Read"
+                books={this.props.booksOnShelf.filter((book) => book.shelf === "wantToRead")}
+                onChange={this.props.onShelfChange}
+                />                
+                <Shelf section="Read"
+                books={this.props.booksOnShelf.filter((book) => book.shelf === "read")}
+                onChange={this.props.onShelfChange}
+                />                
+              </div>
+            </div>
+            <div className="open-search">
+                <Link to='/search'>Add a book</Link>
+            </div>
+        </div>
+		)
+	}
 }
 
 export default ListBooks
