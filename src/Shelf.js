6898b0c071fa6c4f19146b3b6095cbfc1112defc
bhaveshbs789 <bhaveshbs789@gmail.com>
2018-11-20 14:55:24 +0530

    Prop name updated updated passed to Book component

--- src/Shelf.js
+++ src/Shelf.js
@@ -3,14 +3,14 @@ import Book from './Book'
 
 class Shelf extends Component {
 	render() {
-        //console.log('Props from ListBooks', this.props);
+        
 		return (
 		    <div className="bookshelf">
               <h2 className="bookshelf-title">{this.props.section}</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
                   {this.props.books.map((book) => (
-                        <Book key={book.id} bookStatus={book} changeShelf={this.props.onChangeShelf}/>
+                        <Book key={book.id} oneBook={book} changeShelf={this.props.onChange}/>
                     )                
                   )}                  
                 </ol>
