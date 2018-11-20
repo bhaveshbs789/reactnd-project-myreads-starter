9d6928389ea0d0caf5e8d512623e723a80b61757
bhaveshbs789 <bhaveshbs789@gmail.com>
2018-11-20 14:52:05 +0530

    Props updated being passed from Shelf
    Same prop name should be present in Prop and Shelf

--- src/Book.js
+++ src/Book.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 
 class Book extends Component {
 	render() {
-        console.log('Props from Shelf', this.props);
+        console.log('props in book component', this.props);
 		return (            
 			  <li>
 	            <div className="book">
 	              <div className="book-top">
-	                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: "url(" + this.props.bookStatus.imageLinks.thumbnail + ")" }}></div>
+	                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.oneBook.imageLinks.thumbnail}")` }}></div>
 	                <div className="book-shelf-changer">
-	                  <select value={this.props.bookStatus.shelf} onChange={(evt => this.props.changeShelf(this.props.bookStatus, evt.target.value))}>
+	                  <select value={this.props.oneBook.shelf} onChange={(evt => this.props.changeShelf(this.props.oneBook, evt.target.value))}>
 	                    <option value="move" disabled>Move to...</option>
 	                    <option value="currentlyReading">Currently Reading</option>
 	                    <option value="wantToRead">Want to Read</option>
@@ -18,8 +18,8 @@ class Book extends Component {
 	                  </select>
 	                </div>
 	              </div>
-	              <div className="book-title">{this.props.bookStatus.title}</div>
-	              <div className="book-authors">{this.props.bookStatus.authors[0]}</div>
+	              <div className="book-title">{this.props.oneBook.title}</div>
+	              <div className="book-authors">{(this.props.oneBook.authors)}</div>
 	            </div>
 	          </li>
 		)
