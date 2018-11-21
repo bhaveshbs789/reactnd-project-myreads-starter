65c17efdaf4919f9e569520f926836f95cf97b81
bhaveshbs789 <bhaveshbs789@gmail.com>
2018-11-21 15:50:02 +0530

    Changed the onChange event handler
    Event handler onChange for Select element was changed
    A method added changeBookShelf

--- src/Book.js
+++ src/Book.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react'
 
 class Book extends Component {
+    
+    /* 
+    The changeBookShelf method is used to get the value selected from the Drop down
+    when the onchange event is triggered on the select element when it is changed.
+    The value gets stored in event.target.value which is passed as a argument to 
+    the prop "changeShelf".
+    */
+    
+    changeBookShelf = (event) => {
+        this.props.changeShelf(event.target.value)
+    }
+    
 	render() {
-        console.log('props in book component', this.props);
+        
+        //console.log('props in book component', this.props);
+        
 		return (            
 			  <li>
 	            <div className="book">
 	              <div className="book-top">
 	                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.oneBook.imageLinks.thumbnail}")` }}></div>
-	                <div className="book-shelf-changer">
-	                  <select value={this.props.oneBook.shelf} onChange={(evt => this.props.changeShelf(this.props.oneBook, evt.target.value))}>
+	                <div className="book-shelf-changer">	                  
+                      <select value={this.props.oneBook.shelf} onChange={this.changeBookShelf}>            
 	                    <option value="move" disabled>Move to...</option>
 	                    <option value="currentlyReading">Currently Reading</option>
 	                    <option value="wantToRead">Want to Read</option>
