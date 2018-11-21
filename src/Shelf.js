5dfaeb8dd2eb8d24cf4b1ada89298420487d8d4c
bhaveshbs789 <bhaveshbs789@gmail.com>
2018-11-21 15:56:10 +0530

    changeShelf prop updated
    The changeShelf prop for Book component has been updated to reflect the book and shelf as arguments passed into onChange prop via the updateShelf method

--- src/Shelf.js
+++ src/Shelf.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react'
 import Book from './Book'
 
 class Shelf extends Component {
+    
+    updateShelf = (book, shelf) => {
+        this.props.onChange(book, shelf)
+    }
+    
 	render() {
         
-		return (
-		    <div className="bookshelf">
+	    return (
+	    <div className="bookshelf">
               <h2 className="bookshelf-title">{this.props.section}</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
                   {this.props.books.map((book) => (
-                        <Book key={book.id} oneBook={book} changeShelf={this.props.onChange}/>
+                        <Book key={book.id} oneBook={book} changeShelf={(shelf) => this.updateShelf(book, shelf)}/>
                     )                
                   )}                  
                 </ol>
